c53eed38711a63a0adbe95f4d7042ace2bd50b41
SparrowXD-beuh <151991041+SparrowXD-beuh@users.noreply.github.com>
2024-01-01 18:55:56 +0530

    Create database.js

---
+++ src/database.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+
+const database = new MongoClient(`mongodb+srv://admin:bAF3ne5QrjplxVwn@freecluster.7xu0m7g.mongodb.net/?retryWrites=true&w=majority`);
+async function connectToDatabase() {
+    try {
+      await database.connect();
+      console.log("Connected to the database");
+    } catch (error) {
+      console.error("Database Error: ", error.message);
+    }
+};
+
+async function find(imdb_id, collection) {
+    const exists = await database.db("imdb").collection(collection).findOne({ _id: imdb_id });
+    if (exists) return exists;
+    else return false
+};
+
+async function insert(doc, collection) {
+    const ttl = 60;
+    await database.db("imdb").collection(collection).createIndex({ "expiresAt": 1 }, { expireAfterSeconds: ttl });
+    doc.expiresAt = new Date();
+    doc.expiresAt.setSeconds(doc.expiresAt.getSeconds() + ttl);
+    await database.db("imdb").collection(collection).insertOne(doc);
+}
+
+module.exports = { connectToDatabase, find, insert }
