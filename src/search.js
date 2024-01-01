f9dccc2742c4f7af8f827d66ee575891944ca75a
SparrowXD-beuh <151991041+SparrowXD-beuh@users.noreply.github.com>
2024-01-01 18:55:24 +0530

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const headers = require("./config.json")
+const headers = require("./config.json");
+const { insert, find } = require("./database");
 
 const search = async (titleName) => {
     try {
@@ -14,6 +15,10 @@ const search = async (titleName) => {
             imdb_id.push($(element).attr("href").match(/\/title\/(tt\d+)\/\?/)[1]);
         });
 
+        if (imdb_id.length <= 0) {
+            throw new Error("No results found for search with title " + titleName)
+        }
+
         const results = await Promise.all((imdb_id.slice(0, 15)).map(async (id, index) => {
             return await getInfo(id);
         }));
@@ -21,11 +26,14 @@ const search = async (titleName) => {
         return results
     } catch (error) {
         console.error("Error:", error.message);
+        throw error;
     }
 }
 
 const getInfo = async (imdb_id) => {
     try {
+        const exists = await find(imdb_id, "titles");
+        if (exists) return exists;
         const response = await axios.get(`https://www.imdb.com/title/${imdb_id}`, 
             {
                 headers: headers
@@ -33,8 +41,7 @@ const getInfo = async (imdb_id) => {
         );
         const $ = cheerio.load(response.data);
         const title = $("span.hero__primary-text").text().trim();
-        const storyline = await getStoryline(imdb_id);
-        const poster = $("img.ipc-image").attr("src").trim().replace(/UX.*\.jpg/, "UX3000.jpg");
+        const poster = $("img.ipc-image").attr("src").trim().replace(/@@.*$/, "@@._V1_QL75_UX3000.jpg");        ;
         const genres = [];
         $("div.ipc-chip-list__scroller").find("a").each((index, element) => {
             genres.push($(element).text().trim())
@@ -48,22 +55,27 @@ const getInfo = async (imdb_id) => {
         const seasons = $("select#browse-episodes-season").find("option").length - 2;
         const episodes = parseInt($("span.ipc-title__subtext").eq(0).text().trim());
 
-        return {
-            imdb_id,
+        const doc = {
+            _id: imdb_id,
             title,
+            type: seasons <= 0 ? "Movie" : "TV series",
             poster,
-            storyline,
+            storyline: await getStoryline(imdb_id),
+            taglines: await getTaglines(imdb_id),
             genres,
             creator,
             producers,
-            seasons,
-            episodes,
-            rating
-        }
+            seasons: seasons <= 0 ? null : seasons,
+            episodes: seasons <= 0 ? null : episodes,
+            rating,
+        };
+        await insert(doc, "titles");
+        return doc;
     } catch (error) {
-        console.error("Error:", error.message);
+        console.error("Error:", error);
+        throw new Error("Invalid imdb_id");
     }
-}
+};
 
 const getStoryline = async (imdb_id) => {
     try {
@@ -78,30 +90,31 @@ const getStoryline = async (imdb_id) => {
         });
         return textArray
     } catch (error) {
-        console.error("Error:", error.message);
+        console.error("Error in getStoryline():", error.message);
     }
 };
 
-const getTaglines = async () => {
+const getTaglines = async (imdb_id) => {
     try {
-        await axios.get(`https://www.imdb.com/title/${imdb_id}/taglines`, {
-                headers: headers,
-            }).then((res) => {
-            const $ = cheerio.load(res.data);
-            const taglinesArray = [];
-            $("div.ipc-html-content-inner-div").each((index, element) => {
-                const tagline = $(element).text().trim();
-                taglinesArray.push(tagline);
-            });
-            return taglinesArray;
+        const result = await axios.get(`https://www.imdb.com/title/${imdb_id}/taglines`, {
+            headers: headers,
         })
+        const $ = cheerio.load(result.data);
+        const taglinesArray = [];
+        $("div.ipc-html-content-inner-div").each((index, element) => {
+            taglinesArray.push($(element).text().trim());
+        });
+        return taglinesArray;
     } catch (error) {
-        console.error("Error:", error.message);
+        console.error("Error in getTaglines():", error.message);
     }
 };
 
 const getEpisodes = async (imdb_id, season) => {
     try {
+        const exists = await find({imdb_id, season}, "episodes");
+        if (exists) return exists;
+        if (season <= 0) throw new Error("Season cant be 0 or a negative Int");
         const response = await axios.get(`https://www.imdb.com/title/${imdb_id}/episodes/?season=${season}`,
             {
                 headers: headers,
@@ -109,7 +122,7 @@ const getEpisodes = async (imdb_id, season) => {
         );
         const $ = cheerio.load(response.data);
         const episodesArray = [];
-        $("article.episode-item-wrapper").each((index, element) => {
+        await Promise.all($("article.episode-item-wrapper").each((index, element) => {
             episodesArray.push({
                 episode: $(element).find("div.ipc-title__text").text().trim(),
                 overview: $(element).find("div.ipc-html-content-inner-div").text().trim(),
@@ -117,16 +130,24 @@ const getEpisodes = async (imdb_id, season) => {
                 date: $(element).find("span").eq(0).text().trim(),
                 rating: $(element).find("span").eq(1).text().trim(),
             })
-        })
-
-        return {episodes: episodesArray}
+        }))
+        if (episodesArray.length <= 0) throw new Error("No episodes found for season " + season)
+        const doc = {
+            _id: {imdb_id, season},
+            episodes: episodesArray
+        };
+        await insert(doc, "episodes");
+        return doc;
     } catch (error) {
         console.error("Error: ", error.message);
+        throw error;
     }
 };
 
 const getCast = async (imdb_id) => {
     try {
+        const exists = await find(imdb_id, "cast");
+        if (exists) return exists;
         const response = await axios.get(`https://www.imdb.com/title/${imdb_id}/fullcredits`,
             {
                 headers: headers,
@@ -134,7 +155,7 @@ const getCast = async (imdb_id) => {
         );
         const $ = cheerio.load(response.data);
         const castArray = [];
-        $("tr.odd, tr.even").each((index, element) => {
+        await Promise.all($("tr.odd, tr.even").each((index, element) => {
             if ($(element).find("td:eq(1) a").text().trim()) {
                 castArray.push({
                     name: $(element).find("td:eq(1) a").text().trim(),
@@ -143,11 +164,14 @@ const getCast = async (imdb_id) => {
                     screentime: $(element).find("td.character a:eq(1)").text().trim()
                 });
             }
-        });
-
-        return {cast: castArray};
+        }));
+        if (castArray.length <= 0) throw new Error("Coudnt find any cast for imdb_id " + imdb_id)
+        const doc = {_id: imdb_id, cast: castArray};
+        await insert(doc, "cast");
+        return doc;
     } catch (error) {
         console.error("Error fetching cast data:", error.message);
+        throw error
     }
 };
 
